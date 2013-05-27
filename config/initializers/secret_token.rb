4252e33c1682dfdf1792cdafff55d8f230c3df72
Dave Riddle <david@joyvuu.com>
2013-05-27 11:30:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = '29d7f9e8fb2f320289892a60553cceda097f918821cae6fd4a65e769eae1190f72cdc565a2ab50bde0218bbcca9c17bc84ff8353f2452bb72c2e605b078fefcf'
